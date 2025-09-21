da9a0d6a71df1cc0189413ea2a121bd71c330f49
Shanyu Thibaut Juneja <shanyujuneja@gmail.com>
2025-09-20 19:27:18 -0700

    nice touches

--- config/site.ts
+++ config/site.ts
@@ -1,7 +1,7 @@
 export type SiteConfig = typeof siteConfig;
 
 export const siteConfig = {
-  name: "Next.js + HeroUI",
+  name: "Themis.ai - AI Legal",
   description: "Make beautiful websites regardless of your design experience.",
   navItems: [
     {
